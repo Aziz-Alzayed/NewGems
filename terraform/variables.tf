f67ae6bdfe8c2879f64ca39d8bc2aaf4b733e0ea
Aziz-Alzayed <67501412+Aziz-Alzayed@users.noreply.github.com>
2024-04-04 19:13:44 +0300

    adjust Terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -60,3 +60,13 @@ variable "sql_server_credentials" {
     password = string
   })
 }
+
+locals {
+  resource_group_name    = "new-gems-rg"
+  location               = "North Europe"
+  static_web_app_name    = "new-gems-static-web"
+  app_service_name       = "new-gems-app-service"
+  sql_server_name        = "new-gems-sql-server"
+  sql_db_name            = "new-gems-sql-db"
+  app_environment        = "Production"
+}
